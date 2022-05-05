69311a90056bd3d7bbeba1b0d80abbcd77c41854
kagawagao <kingsongao1221@gmail.com>
2022-05-05 10:22:25 +0800

    test: update test case

--- __tests__/loader.spec.ts
+++ __tests__/loader.spec.ts
@@ -4,21 +4,41 @@
 import compiler from './compiler'
 import path from 'path'
 import fs from 'fs'
+import { promisify } from 'util'
+import prettier from 'prettier'
 
+const content = `interface CSSModules {
+   a: string;
+   b: string;
+ }
+ export const cssModules: CSSModules;
+ export default cssModules;`
 const filePath = 'fixtures/example.module.css'
 const noModulesFilePath = 'fixtures/no-modules.css'
 const namedCSSFilePath = 'fixtures/named.module.css'
 const lessFilePath = 'fixtures/less.module.less'
 
-function getDeclarationFilePath(filePath: string) {
+function getDeclarationFilePath(filePath) {
   return path.resolve(__dirname, filePath + '.d.ts')
+}
+
+function format(content: string) {
+  const config = prettier.resolveConfig.sync(__filename)
+  return prettier.format(content, {
+    ...config,
+    parser: 'typescript',
+  })
 }
 
 test('Basic', async () => {
   await compiler(filePath)
-  const { default: cssModules } = await import(getDeclarationFilePath(filePath))
-  expect(cssModules.a).toBeDefined()
-  expect(cssModules.b).toBeDefined()
+  const output = await promisify(fs.readFile)(
+    getDeclarationFilePath(filePath),
+    {
+      encoding: 'utf-8',
+    }
+  )
+  expect(format(output)).toEqual(format(content))
 })
 
 test('Named export', async () => {
@@ -27,10 +47,14 @@ test('Named export', async () => {
       namedExport: true,
     },
   })
+  const output = await promisify(fs.readFile)(
+    getDeclarationFilePath(namedCSSFilePath),
+    {
+      encoding: 'utf-8',
+    }
+  )
 
-  const { cssModules } = await import(getDeclarationFilePath(filePath))
-  expect(cssModules.a).toBeDefined()
-  expect(cssModules.b).toBeDefined()
+  expect(format(output)).toEqual(format(content))
 })
 
 test('No Modules', async () => {
@@ -51,8 +75,11 @@ test('Auto Modules', async () => {
 
 test('CSS Preprocessor', async () => {
   await compiler(lessFilePath)
-
-  const { default: cssModules } = await import(getDeclarationFilePath(filePath))
-  expect(cssModules.a).toBeDefined()
-  expect(cssModules.b).toBeDefined()
+  const output = await promisify(fs.readFile)(
+    getDeclarationFilePath(lessFilePath),
+    {
+      encoding: 'utf-8',
+    }
+  )
+  expect(format(output)).toEqual(format(content))
 })
